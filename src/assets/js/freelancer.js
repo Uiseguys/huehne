2ac68948ce1670b95878f709ee2a2327d3661228
Cris Shaki <cmshaki@gmail.com>
2019-05-20 00:35:29 +0300

    Finished creating the javascript function for drawing the skews in svg

--- src/assets/js/freelancer.js
+++ src/assets/js/freelancer.js
@@ -1,106 +1,102 @@
-(function($) {
-  "use strict"; // Start of use strict
+(function ($) {
+    "use strict";
 
-  // Create svg files that dynamically change with the image size
-  const svgDrawer = () => {
-    try {
-      let h = $("html").height();
-      let w = $("html").width();
+    // Start of use strict
 
-      // height of the slant 12%
-      let ch = .12 * h;
+    // Create svg files that dynamically change with the image size
+    const svgDrawer = () => {
+        try {
+            const wh = [$("body").width(), $("body").height()]; // An array of both width and height of the body
 
-      let svgBottom = `<svg id="bottom-triangle" width="100%" height="100%">
-                  <path d="M0 ${h-ch} L0 ${h} L${w} ${h}Z" fill="white" />
-                </svg>`;
-      let svgTop = `<svg id="bottom-triangle" width="100%" height="100%">
-                  <path d="M0 0 L0 ${ch} L${w} 0Z" fill="white" />
-                </svg>`;
-      $('.bottom-slide').html(svgBottom);
-      $('.top-slide').html(svgTop);
-    } catch(err){
-      console.log(err);
-    } 
-  }
+            // height of the slant 12% of total height
+            const ch = 0.12 * wh[1];
 
-  svgDrawer();
-  $("window").resize(() => {
-    try {
-      svgDrawer();
-    } catch (err) {
-      console.log(err);
-    }
-  });
+            const svgBottom = `<path d="M0 ${wh[1] - ch} L0 ${wh[1]} L${wh[0]} ${wh[1]}Z" fill="white" />`;
+            const svgTop = `<path d="M0 0 L0 ${ch} L${wh[0]} 0Z" fill="white" />`;
+            $(".bottom-slide").html(svgBottom);
+            $(".top-slide").html(svgTop);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 70)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
-  // Scroll to top button appear
-  $(document).scroll(function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 80
-  });
-
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
-  // Modal popup$(function () {
-  $('.portfolio-item').magnificPopup({
-    type: 'inline',
-    preloader: false,
-    focus: '#username',
-    modal: true
-  });
-  $(document).on('click', '.portfolio-modal-dismiss', function(e) {
-    e.preventDefault();
-    $.magnificPopup.close();
-  });
-
-  // Floating label headings for the contact form
-  $(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-      $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-      $(this).removeClass("floating-label-form-group-with-focus");
+    svgDrawer();
+    $(window).resize(() => {
+        try {
+            svgDrawer();
+        } catch (err) {
+            console.log(err);
+        }
     });
-  });
 
-})(jQuery); // End of use strict
+    // Smooth scrolling using jQuery easing
+    $("a.js-scroll-trigger[href*=\"#\"]:not([href=\"#\"])").click(function () {
+        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+            let target = $(this.hash);
+            target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
+            if (target.length) {
+                $("html, body").animate({
+                    scrollTop: (target.offset().top - 70),
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Scroll to top button appear
+    $(document).scroll(function () {
+        const scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+            $(".scroll-to-top").fadeIn();
+        } else {
+            $(".scroll-to-top").fadeOut();
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(() => {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 80,
+    });
+
+    // Collapse Navbar
+    const navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+    // Modal popup$(function () {
+    $(".portfolio-item").magnificPopup({
+        type: "inline",
+        preloader: false,
+        focus: "#username",
+        modal: true,
+    });
+    $(document).on("click", ".portfolio-modal-dismiss", (e) => {
+        e.preventDefault();
+        $.magnificPopup.close();
+    });
+
+    // Floating label headings for the contact form
+    $(() => {
+        $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function () {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function () {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
+    });
+}(jQuery)); // End of use strict

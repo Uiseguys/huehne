c1d60aa6210f1b57dfe46edab295c5ffc59e6b2a
Cris Shaki <cmshaki@gmail.com>
2019-05-21 09:52:45 +0300

    Changed to use local Bootstrap folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
-import "bootstrap/scss/bootstrap.scss";
+import "./assets/scss/bootstrap/bootstrap.scss";
 import "./assets/scss/startbootsrap/freelancer.scss";
 import "./assets/scss/home.scss";
 import "./assets/js/freelancer";

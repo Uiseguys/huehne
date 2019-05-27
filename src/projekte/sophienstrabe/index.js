6112c67a2e87ec1c70dc4842096b5dc18eeeec69
Cris Shaki <cmshaki@gmail.com>
2019-05-27 13:05:42 +0300

    Added the remaining optimized images to the projekte pages

--- src/projekte/sophienstrabe/index.js
+++ src/projekte/sophienstrabe/index.js
@@ -2,6 +2,6 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/js/all";
 import "../../assets/shared/scss/startbootsrap/freelancer.scss";
-import "../../assets/projekte/dorotheenhofe/scss/dorotheenhofe.scss";
+import "../../assets/projekte/sophienstrabe/scss/sophienstrabe.scss";
 import "../../assets/shared/js/skews";
 import "../../assets/shared/js/freelancer";

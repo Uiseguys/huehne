ab0b7e5aeca0181a92dcf79de56bc1f2406aa54e
Cris Shaki <cmshaki@gmail.com>
2019-06-02 16:35:10 +0300

    Language Switch Added to the Navbar

--- src/assets/shared/js/freelancer.js
+++ src/assets/shared/js/freelancer.js
@@ -4,18 +4,18 @@
     // Start of use strict
 
     // Smooth scrolling using jQuery easing
-    $("a.js-scroll-trigger[href*=\"#\"]:not([href=\"#\"])").click(function () {
-        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
-            let target = $(this.hash);
-            target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
-            if (target.length) {
-                $("html, body").animate({
-                    scrollTop: (target.offset().top - 70),
-                }, 1000, "easeInOutExpo");
-                return false;
-            }
-        }
-    });
+    //$("a.js-scroll-trigger[href*=\"#\"]:not([href=\"#\"])").click(function () {
+        //if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+            //let target = $(this.hash);
+            //target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
+            //if (target.length) {
+                //$("html, body").animate({
+                    //scrollTop: (target.offset().top - 70),
+                //}, 1000, "easeInOutExpo");
+                //return false;
+            //}
+        //}
+    //});
 
     // Scroll to top button appear
     //$(document).scroll(function () {
@@ -27,16 +27,16 @@
         //}
     //});
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(() => {
-        $(".navbar-collapse").collapse("hide");
-    });
+    //// Closes responsive menu when a scroll trigger link is clicked
+    //$(".js-scroll-trigger").click(() => {
+        //$(".navbar-collapse").collapse("hide");
+    //});
 
     // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 80,
-    });
+    //$("body").scrollspy({
+        //target: "#mainNav",
+        //offset: 80,
+    //});
 
     // Collapse Navbar
     const navbarCollapse = () => {

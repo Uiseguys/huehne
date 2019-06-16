191b5c6c59c023979594516fa71e1384a9056bb2
Cris Shaki <cmshaki@gmail.com>
2019-06-16 20:21:17 +0300

    Internet Explorer Support

--- src/assets/shared/js/skews.js
+++ src/assets/shared/js/skews.js
@@ -5,25 +5,72 @@
 // Adding SVG Elements to the DOM
 const createSvgDomElements = () => {
     try {
-        const mainInner = document.querySelector(".main-section > .container"); // Main Section bottom left skew
+        // Retrieving DOM elements that will have skews added
+        const slide = document.querySelector("section > .section-slide-1");
+        const body = document.querySelector("body");
+        let wh = [body.getBoundingClientRect().width, body.getBoundingClientRect().height]; // An array of both width and height of the body
+        // height of the slant 60px + 4 adjusting for margins
+        let ch = 60 + 4;
+        
+        // Paths to be added to SVG elements
+        const svgBottomLeft = `<path d="M0 ${wh[1] - (ch / 2)} L0 ${wh[1] + 4} L${wh[0]} ${wh[1] + 4} L${wh[0]} ${wh[1]}Z" fill="white"></path>`;
+        const svgBottomRight = `<path d="M0 ${wh[1]} L0 ${wh[1] + 4} L${wh[0]} ${wh[1] + 4} L${wh[0]} ${wh[1] - (ch / 2)}Z" fill="white"></path>`;
+        const svgTopLeft = `<path d="M0 0 L0 ${ch + 4} L${wh[0]} 4 L${wh[0]} 0Z" fill="white"></path>`;
+
+        // Creating DOM elements
+        const main = document.querySelector(".main-section .container"); // Main Section bottom left skew
         const pro = document.querySelector(".projekte");
-        const slide = document.querySelector(".section-slide-1");
+        let el;
 
         // Main Section SVG Element
-        mainInner.insertAdjacentHTML("beforebegin", "<svg class='bottom-left-skew'><svg>");
+        if (main.parentNode.querySelector(".bottom-left-skew") == null) {
+            main.insertAdjacentHTML("beforebegin", `<svg class='bottom-left-skew'>${svgBottomLeft}</svg>`);
+        } else {
+            el = main.parentNode.querySelector(".main-section > .bottom-left-skew");
+            main.parentNode.removeChild(el);
+            main.insertAdjacentHTML("beforebegin", `<svg class='bottom-left-skew'>${svgBottomLeft}</svg>`);
+        }
 
         // Index Pages Projekte Sections SVG elements
         if (pro != null) {
+            const col = document.getElementById("projekte-slide-1").getBoundingClientRect();
+            const bw = body.getBoundingClientRect().width;
+            wh = [col.width, col.height]; // An array of both width and height of the projekte elements
+            
+            // height of the slant 60px + 2 accounting for margins
+            ch = 60 + 4;
+
+            // Svg Path Strings
+            const svgProjekteBottomLeft = `<path d="M0 ${wh[1] - (ch / 2)} L0 ${wh[1] + 4} L${wh[0]} ${wh[1] + 4} L${wh[0]} ${wh[1]}Z" fill="#f5f5f5"></path>`;
+            const svgProjekteBottomRight = `<path d="M0 ${wh[1]} L0 ${wh[1] + 4} L${wh[0]} ${wh[1] + 4} L${wh[0]} ${wh[1] - ch}Z" fill="#f5f5f5"></path>`;
+            const svgProjekteTopLeft = `<path d="M0 0 L0 ${ch} L${wh[0]} 4 L${wh[0]} 0Z" fill="#f5f5f5"></path>`;
+            const svgProjekteTopRight = `<path d="M0 0 L0 4 L${wh[0]} ${ch / 2} L${wh[0]} 0Z" fill="#f5f5f5"></path>`;
+            //
             // Creates first svg element for the first projekte section
             const firstPro = document.querySelector(".projekte:nth-child(4)");
-            firstPro.querySelector(".container").insertAdjacentHTML("beforebegin", "<svg class='top-left-skew'><svg>");
+
+            if (firstPro.querySelector(".top-left-skew") == null) {
+                firstPro.insertAdjacentHTML("afterbegin", `<svg class='top-left-skew'>${svgTopLeft}</svg>`);
+            } else {
+                const el = firstPro.querySelector(".top-left-skew");
+                firstPro.removeChild(el);
+                firstPro.insertAdjacentHTML("afterbegin", `<svg class='top-left-skew'>${svgTopLeft}</svg>`);
+            }
+
             // Creates svg elements for first projekte image
-            firstPro.querySelector(".projekte-wrapper .row .col-lg-6#projekte-slide-1").innerHTML = "<svg class='projekte-bottom-left-skew'></svg>";
+            firstPro.querySelector(".projekte-wrapper .row .col-lg-6#projekte-slide-1").innerHTML = `<svg class='projekte-bottom-left-skew'>${svgProjekteBottomLeft}</svg>`;
 
             // Creates svg elements for the last projekte section
-            const lastPro = document.querySelector(".projekte:nth-child(14) > .container");
-            lastPro.insertAdjacentHTML("beforebegin", "<svg class='bottom-left-skew'><svg>");
+            const lastPro = document.querySelector(".projekte:nth-child(14)");
+            const svgBtmLeft = `<path d="M0 ${wh[1] - (ch / 2)} L0 ${wh[1]} L${bw} ${wh[1]}Z" fill="white" />`;
 
+            if (lastPro.querySelector(".bottom-left-skew") == null) {
+                lastPro.insertAdjacentHTML("afterbegin", `<svg class='bottom-left-skew'>${svgBtmLeft}</svg>`);
+            } else {
+                let el = lastPro.querySelector(".bottom-left-skew");
+                lastPro.removeChild(el);
+                lastPro.insertAdjacentHTML("afterbegin", `<svg class='bottom-left-skew'>${svgBtmLeft}</svg>`);
+            }
             // Creates SVG elements excluding the first projekte section
             let selector;
             let el;
@@ -31,15 +78,18 @@ const createSvgDomElements = () => {
                 selector = `.projekte > .row > .col-lg-6#projekte-slide-${i}`;
                 el = document.querySelector(selector);
                 if (i % 2 === 0) { // Check if even numbered to have right sided angled skews
-                    el.innerHTML = "<svg class='projekte-top-right-skew'></svg><svg class='projekte-bottom-right-skew'></svg>";
+                    el.innerHTML = `<svg class='projekte-top-right-skew'>${svgProjekteTopRight}</svg><svg class='projekte-bottom-right-skew'>${svgProjekteBottomRight}</svg>`;
                 } else { // If not will have left sided angled skews
-                    el.innerHTML = "<svg class='projekte-top-left-skew'></svg><svg class='projekte-bottom-left-skew'></svg>";
+                    el.innerHTML = `<svg class='projekte-top-left-skew'>${svgProjekteTopLeft}</svg><svg class='projekte-bottom-left-skew'>${svgProjekteBottomLeft}</svg>`;
                 }
             }
         }
 
         // Check if projekte product classes exist and create svg elements
         if (slide != null) {
+            let el;
+            const ss = slide.getBoundingClientRect().height; // Slide section height
+            const svgSlideBtmLeft = `<path d="M0 ${ss - (ch / 2)} L0 ${ss + 2} L${wh[0]} ${ss + 2} L${wh[0]} ${ss}Z" fill="white" />`;
             // Retrieve the other sections
             const slideTwo = document.querySelector(".section-slide-2");
             const slideThree = document.querySelector(".section-slide-3");
@@ -50,10 +100,12 @@ const createSvgDomElements = () => {
 
             if (slideTwo != null) {
                 document.querySelector("section:nth-child(5)").setAttribute("class", "height-100");
-                if (slide.querySelector(".row") == null) {
-                    slide.insertAdjacentHTML("beforebegin", "<svg class='top-left-skew'></svg>");
+                if (slide.parentNode.querySelector(".top-left-skew") == null) {
+                    slide.insertAdjacentHTML("beforebegin", `<svg class='top-left-skew'>${svgTopLeft}</svg>`);
                 } else {
-                    slide.querySelector(".row").insertAdjacentHTML("beforebegin", "<svg class='top-left-skew'></svg>");
+                    el = slide.parentNode.querySelector(".top-left-skew");
+                    slide.parentNode.removeChild(el);
+                    slide.insertAdjacentHTML("beforebegin", `<svg class='top-left-skew'>${svgTopLeft}</svg>`);
                 }
                 slideTwo.setAttribute("class", "section-slide-2 no-top");
                 if (slideThree != null) {
@@ -62,19 +114,45 @@ const createSvgDomElements = () => {
                     if (slideFour != null) {
                         document.querySelector("section:nth-child(7)").setAttribute("class", "height-100");
                         slideFour.setAttribute("class", "section-slide-4 no-top");
-                        slideFour.insertAdjacentHTML("beforebegin", "<svg class='projekte-page-bottom-left-skew'></svg>");
+                        if (slideFour.parentNode.querySelector(".projekte-page-bottom-left-skew") == null) {
+                            slideFour.insertAdjacentHTML("beforebegin", `<svg class='projekte-page-bottom-left-skew'>${svgSlideBtmLeft}</svg>`);
+                        } else {
+                            el = slideFour.parentNode.querySelector(".projekte-page-bottom-left-skew");
+                            slideFour.parentNode.removeChild(el);
+                            slideFour.insertAdjacentHTML("beforebegin", `<svg class='projekte-page-bottom-left-skew'>${svgSlideBtmLeft}</svg>`);
+                        }
                     } else {
-                        slideThree.insertAdjacentHTML("beforebegin", "<svg class='projekte-page-bottom-left-skew'></svg>");
+                        if (slideThree.parentNode.querySelector(".projekte-page-bottom-left-skew") == null) {
+                            slideThree.insertAdjacentHTML("beforebegin", `<svg class='projekte-page-bottom-left-skew'>${svgSlideBtmLeft}</svg>`);
+                        } else {
+                            el = slideThree.parentNode.querySelector(".projekte-page-bottom-left-skew");
+                            slideThree.parentNode.removeChild(el);
+                            slideThree.insertAdjacentHTML("beforebegin", `<svg class='projekte-page-bottom-left-skew'>${svgSlideBtmLeft}</svg>`);
+                        }
                     }
                 } else {
-                    slideTwo.insertAdjacentHTML("beforebegin", "<svg class='projekte-page-bottom-left-skew'></svg>");
+                    if (slideTwo.parentNode.querySelector(".projekte-page-bottom-left-skew") == null) {
+                        slideTwo.insertAdjacentHTML("beforebegin", `<svg class='projekte-page-bottom-left-skew'>${svgSlideBtmLeft}</svg>`);
+                    } else {
+                        el = slideTwo.parentNode.querySelector(".projekte-page-bottom-left-skew");
+                        slideTwo.parentNode.removeChild(el);
+                        slideTwo.insertAdjacentHTML("beforebegin", `<svg class='projekte-page-bottom-left-skew'>${svgSlideBtmLeft}</svg>`);
+                    }
                 }
             } else {
-                slide.insertAdjacentHTML("beforebegin", "<svg class='top-left-skew'></svg><svg class='projekte-page-bottom-left-skew'></svg>");
+                if (slide.parentNode.querySelector(".projekte-page-bottom-left-skew") == null) {
+                    slide.insertAdjacentHTML("beforebegin", `<svg class='top-left-skew'>${svgTopLeft}</svg><svg class='projekte-page-bottom-left-skew'>${svgSlideBtmLeft}</svg>`);
+                } else {
+                    el = slide.parentNode.querySelector(".projekte-page-bottom-left-skew");
+                    slide.parentNode.removeChild(el);
+                    el = slide.parentNode.querySelector(".top-left-skew");
+                    slide.parentNode.removeChild(el);
+                    slide.insertAdjacentHTML("beforebegin", `<svg class='top-left-skew'>${svgTopLeft}</svg><svg class='projekte-page-bottom-left-skew'>${svgSlideBtmLeft}</svg>`);
+                }
             }
         }
     } catch (err) {
-        console.log(err);
+        console.log(err.message);
     }
 };
 
@@ -93,12 +171,12 @@ const svgDrawer = () => {
         const ch = 60 + 4;
 
         // Paths to be added to SVG elements
-        const svgBottomLeft = `<path d="M0 ${wh[1] - (ch / 2)} L0 ${wh[1] + 4} L${wh[0]} ${wh[1] + 4} L${wh[0]} ${wh[1]}Z" fill="white" />`;
-        const svgBottomRight = `<path d="M0 ${wh[1]} L0 ${wh[1] + 4} L${wh[0]} ${wh[1] + 4} L${wh[0]} ${wh[1] - (ch / 2)}Z" fill="white" />`;
-        const svgTopLeft = `<path d="M0 0 L0 ${ch + 4} L${wh[0]} 4 L${wh[0]} 0Z" fill="white" />`;
+        const svgBottomLeft = `<path d="M0 ${wh[1] - (ch / 2)} L0 ${wh[1] + 4} L${wh[0]} ${wh[1] + 4} L${wh[0]} ${wh[1]}Z" fill="white"></path>`;
+        const svgBottomRight = `<path d="M0 ${wh[1]} L0 ${wh[1] + 4} L${wh[0]} ${wh[1] + 4} L${wh[0]} ${wh[1] - (ch / 2)}Z" fill="white"></path>`;
+        const svgTopLeft = `<path d="M0 0 L0 ${ch + 4} L${wh[0]} 4 L${wh[0]} 0Z" fill="white"></path>`;
 
         // Change svg elements inner HTML with Path Strings
-        main.querySelector(".bottom-left-skew").innerHTML = svgBottomLeft;
+        main.querySelector(".bottom-left-skew").innerHTML.replace("<path>", svgBottomLeft);
         if (projekte != null) {
             // Rendering for bottom left skew on last projekte section
             // Height needs to be changed to accomodate the smaller projekte sections
@@ -142,27 +220,31 @@ const svgProjekteDrawer = () => {
         const ch = 60 + 4;
 
         // Svg Path Strings
-        const svgBottomLeft = `<path d="M0 ${wh[1] - (ch / 2)} L0 ${wh[1] + 4} L${wh[0]} ${wh[1] + 4} L${wh[0]} ${wh[1]}Z" fill="#f5f5f5" />`;
-        const svgBottomRight = `<path d="M0 ${wh[1]} L0 ${wh[1] + 4} L${wh[0]} ${wh[1] + 4} L${wh[0]} ${wh[1] - ch}Z" fill="#f5f5f5" />`;
-        const svgTopLeft = `<path d="M0 0 L0 ${ch} L${wh[0]} 4 L${wh[0]} 0Z" fill="#f5f5f5" />`;
-        const svgTopRight = `<path d="M0 0 L0 4 L${wh[0]} ${ch / 2} L${wh[0]} 0Z" fill="#f5f5f5" />`;
+        const svgBottomLeft = `<path d="M0 ${wh[1] - (ch / 2)} L0 ${wh[1] + 4} L${wh[0]} ${wh[1] + 4} L${wh[0]} ${wh[1]}Z" fill="#f5f5f5"></path>`;
+        const svgBottomRight = `<path d="M0 ${wh[1]} L0 ${wh[1] + 4} L${wh[0]} ${wh[1] + 4} L${wh[0]} ${wh[1] - ch}Z" fill="#f5f5f5"></path>`;
+        const svgTopLeft = `<path d="M0 0 L0 ${ch} L${wh[0]} 4 L${wh[0]} 0Z" fill="#f5f5f5"></path>`;
+        const svgTopRight = `<path d="M0 0 L0 4 L${wh[0]} ${ch / 2} L${wh[0]} 0Z" fill="#f5f5f5"></path>`;
 
         // Add SVG path to class
-        body.querySelectorAll(".projekte-top-left-skew").forEach((item) => {
-            item.innerHTML = svgTopLeft;
-        });
-        body.querySelectorAll(".projekte-bottom-left-skew").forEach((item) => {
-            item.innerHTML = svgBottomLeft;
-        });
-        body.querySelectorAll(".projekte-top-right-skew").forEach((item) => {
-            item.innerHTML = svgTopRight;
-        });
-        body.querySelectorAll(".projekte-bottom-right-skew").forEach((item) => {
-            item.innerHTML = svgBottomRight;
-        });
-        body.querySelectorAll(".projekte-right-skews").forEach((item) => {
-            item.innerHTML = svgTopLeft + svgBottomLeft;
-        });
+        const topLeft = body.querySelectorAll(".projekte-top-left-skew");
+        const bottomLeft = body.querySelectorAll(".projekte-bottom-left-skew");
+        const topRight = body.querySelectorAll(".projekte-top-right-skew");
+        const bottomRight = body.querySelectorAll(".projekte-bottom-right-skew");
+
+        for (let i = 0; i < topLeft.length; i += 1) { 
+            let el = topLeft[i].querySelector("svg path");
+            topLeft[i].removeChild(el);
+            topLeft[i].innerHTML = svgTopLeft;
+        }
+        for (let i = 0; i < bottomLeft.length; i += 1) { 
+            bottomLeft[i].innerHTML = svgBottomLeft;
+        }
+        for (let i = 0; i < topRight.length; i += 1) { 
+            topRight[i].innerHTML = svgTopRight;
+        }
+        for (let i = 0; i < bottomRight.length; i += 1) { 
+            bottomRight[i].innerHTML = svgBottomRight;
+        }
     } catch (err) {
         console.log(err);
     }
@@ -172,20 +254,11 @@ const svgProjekteDrawer = () => {
 const runSvgAlt = () => {
     try {
         createSvgDomElements();
-        svgDrawer();
-        // Check to see if the projekte sections are present in the current viewed page
-        if (document.querySelector(".projekte") != null) {
-            svgProjekteDrawer();
-        }
 
         // Run the functions when the windows are resized
         window.addEventListener("resize", () => {
             try {
-                svgDrawer();
-                // Check to see if the projekte sections are present in the current viewed page
-                if (document.querySelector(".projekte") != null) {
-                    svgProjekteDrawer();
-                }
+                createSvgDomElements();
             } catch (err) {
                 console.log(err);
             }
@@ -200,7 +273,8 @@ const checkBrowserRender = () => {
     let tem,
         M = ua.match(/(opera|chrome|safari|firefox|msie|trident|edge(?=\/))\/?\s*(\d+)(\.\d+)+/i) || [];
     if (/trident/i.test(M[1])) {
-        tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+
+        console.log("This is Internet Explorer");
         return runSvgAlt(); // Internet Explorer present
     }
     if (M[1] === "Chrome") {
@@ -254,7 +328,7 @@ const checkBrowserRender = () => {
         }
         return null;
     }
-}
+};
 
 checkBrowserRender();
 
